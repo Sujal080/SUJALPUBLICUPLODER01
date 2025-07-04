f70fc33a641a8c628021997a15387ab614a3abf1
SUJAL CHAVDA <chavdasujal08@gmail.com>
2025-07-04 17:02:47 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -6,18 +6,16 @@ from os import environ
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "8118667253"))
+OWNER = int(environ.get("OWNER", "8118667253"))  # Optional: still used maybe for logs or admin
 CREDIT = "✿𝐒𝐔𝐉𝐀𝐋✿"
 
-AUTH_USER = os.environ.get('AUTH_USERS', '8118667253').split(',')
-AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
-if int(OWNER) not in AUTH_USERS:
-    AUTH_USERS.append(int(OWNER))
+# 🟢 PUBLIC ACCESS: sabko allow kiya gaya hai
+AUTH_USERS = []  # Empty list: koi restriction nahi rahegi
 
 # ➕ MongoDB config (Render env se aayega)
 MONGO_URI = environ.get("MONGO_URI", "")
 DB_NAME = environ.get("DB_NAME", "Telegrambot")
 COLLECTION_NAME = environ.get("COLLECTION_NAME", "Telegrambot")
-  
+
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
